ad1fa2b4f76466326f5c744ffbd594cbfdae068a
Ravi <ravi.kiran2369@gmail.com>
2023-07-04 17:07:16 +0530

    Intial Commit

---
+++ src/main/java/com/rtech/RtechApplication.java
@@ -0,0 +1,13 @@
+package com.rtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtechApplication.class, args);
+	}
+
+}
